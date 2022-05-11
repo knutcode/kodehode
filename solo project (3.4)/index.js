30d23e9d412d0adf49e20dbba8e0cf318b33dfbb
kodehodeknut <104001548+kodehodeknut@users.noreply.github.com>
2022-05-11 18:18:34 +0200

    Add files via upload

---
+++ solo project (3.4)/index.js
@@ -0,0 +1,23 @@
+let pwdOne = document.getElementById("pwd-el-one");
+let pwdTwo = document.getElementById("pwd-el-two");
+let pwdThree = document.getElementById("pwd-el-three");
+let pwdFour = document.getElementById("pwd-el-four");
+
+function genPassword() {
+  let characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let passwordLength = document.getElementById("slider");
+  let password = "";
+
+  for (let i = 1; i <= passwordLength.value; i++) {
+    let randomNumber = Math.floor(Math.random() * characters.length);
+    password += characters.substring(randomNumber, randomNumber + 1);
+  }
+  return password;
+}
+
+function renderPassword() {
+  document.getElementById("pwd-el-one").value = genPassword();
+  document.getElementById("pwd-el-two").value = genPassword();
+  document.getElementById("pwd-el-three").value = genPassword();
+  document.getElementById("pwd-el-four").value = genPassword();
+}
