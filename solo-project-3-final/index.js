80ffa0e7a847ff65dd45159f2bd689306e7684d8
kodehodeknut <knutc.kodehode@gmail.com>
2022-06-02 19:00:33 +0200

    updated invoice

--- solo-project-3-final/index.js
+++ solo-project-3-final/index.js
@@ -3,48 +3,79 @@ let carBtn = document.getElementById("car-btn");
 let lawnBtn = document.getElementById("lawn-btn");
 let weedsBtn = document.getElementById("weeds-btn");
 let sendBtn = document.getElementById("send-btn");
+let mainContainer = document.getElementById("main-container");
 
 //display values
 let renderService = document.getElementById("render-service");
 let renderTotal = document.getElementById("render-total");
 
 let serviceArray = [];
+let valueArray = [];
 
 let wash = {
   service: "Wash Car",
   value: 10,
 };
-
 let mow = {
   service: "Mown Lawn",
   value: 20,
 };
-
 let pull = {
   service: "Pull weeds",
   value: 30,
 };
 
-carBtn.addEventListener("click", () => {
-  serviceArray.push(`<div><p>${wash.service}</p> <button>Remove</button> <p class="price">$${wash.value}</p></div>`);
-  carBtn.disabled = true;
-  console.log(serviceArray);
-});
+function pushTo(objSer, objVal) {
+  serviceArray.push(`<div><p>${objSer.service}</p> <button>Remove</button> <p class="price">$${objVal.value}</p></div>`);
+}
 
-lawnBtn.addEventListener("click", () => {
-  serviceArray.push(`<div><p>${mow.service}</p> <button>Remove</button> <p class="price">$${mow.value}</p></div>`);
-  lawnBtn.disabled = true;
+function displayService() {
+  renderService.innerHTML = serviceArray.join(" ");
   console.log(serviceArray);
-});
+}
 
-weedsBtn.addEventListener("click", () => {
-  serviceArray.push(`<div><p>${pull.service}</p> <button>Remove</button> <p class="price">$${pull.value}</p></div>`);
-  weedsBtn.disabled = true;
-  console.log(serviceArray);
-});
+function displayTotal() {
+  const sum = valueArray.reduce((partialSum, a) => partialSum + a, 0);
+  renderTotal.textContent = `$${sum}`;
+}
 
+function resetAll() {
+  serviceArray = [];
+  valueArray = [];
+  renderService.innerHTML = "";
+  displayTotal();
+  carBtn.disabled = false;
+  lawnBtn.disabled = false;
+  weedsBtn.disabled = false;
+}
 //example for later
 // wash.totalValue = wash.value + mow.value;
 // console.log(wash.totalValue);
 
-let randomCalc = serviceArray;
+carBtn.addEventListener("click", () => {
+  valueArray.push(wash.value);
+  pushTo(wash, wash);
+  displayService();
+  displayTotal();
+  carBtn.disabled = true;
+});
+
+lawnBtn.addEventListener("click", () => {
+  valueArray.push(mow.value);
+  pushTo(mow, mow);
+  displayService();
+  displayTotal();
+  lawnBtn.disabled = true;
+});
+
+weedsBtn.addEventListener("click", () => {
+  valueArray.push(pull.value);
+  pushTo(pull, pull);
+  displayService();
+  displayTotal();
+  weedsBtn.disabled = true;
+});
+
+sendBtn.addEventListener("click", () => {
+  resetAll();
+});
