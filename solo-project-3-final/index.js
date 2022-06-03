1e13b4f5f3da9a2fca3504e8327e41878cd611ff
kodehodeknut <knutc.kodehode@gmail.com>
2022-06-03 09:36:30 +0200

    cleaned up js + fixed minor spelling mistake

--- solo-project-3-final/index.js
+++ solo-project-3-final/index.js
@@ -1,33 +1,22 @@
-//buttons
 let carBtn = document.getElementById("car-btn");
 let lawnBtn = document.getElementById("lawn-btn");
 let weedsBtn = document.getElementById("weeds-btn");
 let sendBtn = document.getElementById("send-btn");
 let mainContainer = document.getElementById("main-container");
 
-//display values
 let renderService = document.getElementById("render-service");
 let renderTotal = document.getElementById("render-total");
 
 let serviceArray = [];
 let valueArray = [];
 
-let wash = {
-  service: "Wash Car",
-  value: 10,
-};
-let mow = {
-  service: "Mown Lawn",
-  value: 20,
-};
-let pull = {
-  service: "Pull weeds",
-  value: 30,
-};
+let wash = { service: "Wash Car", value: 10 };
+let mow = { service: "Mow Lawn", value: 20 };
+let pull = { service: "Pull weeds", value: 30 };
 
 function pushTo(objSer, objVal) {
   serviceArray.push(
-    `<div><p>${objSer.service}</p> <button>Remove</button> <p class="price"><span>$</span>${objVal.value}</p></div>`
+    `<div><p>${objSer.service}</p> <button onclick="remove()">Remove</button> <p class="price"><span>$</span>${objVal.value}</p></div>`
   );
 }
 
@@ -39,16 +28,6 @@ function displayService() {
 function displayTotal() {
   const sum = valueArray.reduce((partialSum, a) => partialSum + a, 0);
   renderTotal.textContent = `$${sum}`;
-}
-
-function resetAll() {
-  serviceArray = [];
-  valueArray = [];
-  renderService.innerHTML = "";
-  displayTotal();
-  carBtn.disabled = false;
-  lawnBtn.disabled = false;
-  weedsBtn.disabled = false;
 }
 
 carBtn.addEventListener("click", () => {
@@ -74,6 +53,16 @@ weedsBtn.addEventListener("click", () => {
   displayTotal();
   weedsBtn.disabled = true;
 });
+
+function resetAll() {
+  serviceArray = [];
+  valueArray = [];
+  renderService.innerHTML = "";
+  displayTotal();
+  carBtn.disabled = false;
+  lawnBtn.disabled = false;
+  weedsBtn.disabled = false;
+}
 
 sendBtn.addEventListener("click", () => {
   resetAll();
