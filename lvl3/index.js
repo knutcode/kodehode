e0af45a385a0ee7012ce28564c5be34bbaf97b48
kodehodeknut <knutc.kodehode@gmail.com>
2022-06-08 21:25:06 +0200

    removed delay until drum can get hit again

--- lvl3/index.js
+++ lvl3/index.js
@@ -1,34 +1,42 @@
 document.addEventListener("keydown", (e) => {
   if (e.key == "q") {
     document.getElementById("clap").play();
+    document.getElementById("clap").currentTime = 0;
     console.log(`Key "${e.key}"`);
   }
   if (e.key == "w") {
     document.getElementById("hihat").play();
+    document.getElementById("hihat").currentTime = 0;
     console.log(`Key "${e.key}"`);
   }
   if (e.key == "e") {
     document.getElementById("kick").play();
+    document.getElementById("kick").currentTime = 0;
     console.log(`Key "${e.key}"`);
   }
   if (e.key == "r") {
     document.getElementById("openhat").play();
+    document.getElementById("openhat").currentTime = 0;
     console.log(`Key "${e.key}"`);
   }
   if (e.key == "u") {
     document.getElementById("ride").play();
+    document.getElementById("ride").currentTime = 0;
     console.log(`Key "${e.key}"`);
   }
   if (e.key == "i") {
     document.getElementById("snare").play();
+    document.getElementById("snare").currentTime = 0;
     console.log(`Key "${e.key}"`);
   }
   if (e.key == "o") {
     document.getElementById("tink").play();
+    document.getElementById("tink").currentTime = 0;
     console.log(`Key "${e.key}"`);
   }
   if (e.key == "p") {
     document.getElementById("tom").play();
+    document.getElementById("tom").currentTime = 0;
     console.log(`Key "${e.key}"`);
   }
 });
