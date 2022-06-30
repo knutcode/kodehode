ad7e85e44d6d62f257bafe6dce16b0d6c69b125e
kodehodeknut <knutc.kodehode@gmail.com>
2022-06-30 09:10:25 +0200

    modified lines in js

--- pokemon-api/index.js
+++ pokemon-api/index.js
@@ -11,7 +11,6 @@ async function getAllPokemon() {
   let number = Math.floor(Math.random() * 150) + 1;
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
   let data = await response.json();
-  console.log(data);
 
   let pokeNameContainer = document.createElement("section");
   let pokeTypeContainer = document.createElement("div");
@@ -21,6 +20,7 @@ async function getAllPokemon() {
   let img = document.createElement("img");
 
   pokeTypeContainer.classList.add("types");
+  img.src = data.sprites.other["official-artwork"].front_default;
 
   let stringId = data.id.toString();
   let paddedId = stringId.padStart(3, "0");
@@ -28,7 +28,6 @@ async function getAllPokemon() {
   pokeName.textContent = data.name;
   pokeId.textContent = `#${paddedId}`;
   type.textContent = data.types[0].type.name;
-  img.src = data.sprites.other["official-artwork"].front_default;
 
   pokeTypeContainer.append(type);
   pokeNameContainer.append(pokeName, pokeTypeContainer);
@@ -43,26 +42,25 @@ async function getAllPokemon() {
 
   if (type.textContent === data.types[0].type.name) {
     type.classList.add(`${data.types[0].type.name}`);
-    cardBg.id = `${data.types[0].type.name}`;
     btn.classList.add(`${data.types[0].type.name}`);
+    cardBg.id = `${data.types[0].type.name}`;
   }
 
   const fetchNew = await fetch("https://raw.githubusercontent.com/Jeglerjeg/pcbot/master/plugins/pokedexlib/pokedex.json");
   const newData = await fetchNew.json();
 
-  let heightHeader = document.createElement("h2");
-  let pokeHeight = document.createElement("p");
-  let pokeWeight = document.createElement("p");
-  let pokeAbilities = document.createElement("p");
   let desHeader = document.createElement("h2");
+  let pokeDes = document.createElement("p");
+  let bodyHeader = document.createElement("h2");
+  let pokeBody = document.createElement("p");
   let abiHeader = document.createElement("h2");
-  let pokemonDes = document.createElement("p");
+  let pokeAbilities = document.createElement("p");
+
   let description = newData.pokemon[data.name].description;
   let abilitiesArr = data.abilities;
 
-  pokeHeight.textContent = `Height: ${data.height / 10}m | Weight: ${data.weight / 10}kg`;
-  pokeWeight.textContent = `${data.weight / 10}kg`;
-  console.log(abilitiesArr);
+  pokeBody.textContent = `Height: ${data.height / 10}m | Weight: ${data.weight / 10}kg`;
+
   for (i = 0; i < abilitiesArr.length; i++) {
     let textV2 = data.abilities[i].ability.name;
     pokeAbilities.textContent += `${textV2}, `;
@@ -72,11 +70,10 @@ async function getAllPokemon() {
   let newText = stringText.slice(0, -2);
   pokeAbilities.textContent = newText;
 
-  pokemonDes.textContent = description;
   desHeader.textContent = "About";
+  pokeDes.textContent = description;
+  bodyHeader.textContent = "Body";
   abiHeader.textContent = "Abilities";
-  heightHeader.textContent = "Body";
-  // weightHeader.textContent = "Weight";
 
-  textArea.append(desHeader, pokemonDes, heightHeader, pokeHeight, abiHeader, pokeAbilities);
+  textArea.append(desHeader, pokeDes, bodyHeader, pokeBody, abiHeader, pokeAbilities);
 }
